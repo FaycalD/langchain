9ba2895fdaa73151765dbecade33d598867f162d
Xin Liu <sam@secondstate.io>
2023-11-29 07:07:09 +0000

    chore: update integration test
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- libs/langchain/tests/integration_tests/chat_models/test_wasm_chat.py
+++ libs/langchain/tests/integration_tests/chat_models/test_wasm_chat.py
@@ -6,12 +6,30 @@ def test_chat_wasm() -> None:
     model_file = (
         "/home/ubuntu/workspace/wasm-llm/wasm-chat/tinyllama-1.1b-chat-v0.3.Q5_K_M.gguf"
     )
-    wasm_file = "/home/ubuntu/workspace/wasm-llm/wasm-infer/target/wasm32-wasi/release/wasm_infer.wasm"
 
     chat = ChatWasm(
         model_file=model_file,
-        wasm_file=wasm_file,
         prompt_template=PromptTemplateType.ChatML,
+    )
+    system_message = SystemMessage(content="You are an AI assistant")
+    user_message = HumanMessage(content="What is the capital of France?")
+    messages = [system_message, user_message]
+    chat_result = chat(messages)
+    assert isinstance(chat_result, AIMessage)
+    assert isinstance(chat_result.content, str)
+    assert "Paris" in chat_result.content
+
+
+def test_chat_wasm_with_wasm_file() -> None:
+    model_file = (
+        "/home/ubuntu/workspace/wasm-llm/wasm-chat/tinyllama-1.1b-chat-v0.3.Q5_K_M.gguf"
+    )
+    wasm_file = "/home/ubuntu/.wasmedge/wasm/wasm_infer.wasm"
+
+    chat = ChatWasm(
+        model_file=model_file,
+        prompt_template=PromptTemplateType.ChatML,
+        wasm_file=wasm_file,
     )
     system_message = SystemMessage(content="You are an AI assistant")
     user_message = HumanMessage(content="What is the capital of France?")
