d5ebfff4323429944dd66323d86eb420adb61f62
Xin Liu <sam@secondstate.io>
2023-11-30 07:23:54 +0000

    chore: update integration test
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- libs/langchain/tests/integration_tests/chat_models/test_wasm_chat.py
+++ libs/langchain/tests/integration_tests/chat_models/test_wasm_chat.py
@@ -38,3 +38,21 @@ def test_chat_wasm_with_wasm_file() -> None:
     assert isinstance(chat_result, AIMessage)
     assert isinstance(chat_result.content, str)
     assert "Paris" in chat_result.content
+
+
+def test_chat_wasm_with_reverse_prompt() -> None:
+    model_file = "/home/ubuntu/workspace/models/second-state/MistralLite-7B-GGUF/mistrallite.Q5_K_M.gguf"
+
+    chat = ChatWasm(
+        model_file=model_file,
+        prompt_template=PromptTemplateType.MistralLite,
+        reverse_prompt="</s>",
+    )
+    system_message = SystemMessage(content="You are an AI assistant")
+    user_message = HumanMessage(content="What is the capital of France?")
+    messages = [system_message, user_message]
+    chat_result = chat(messages)
+    print(f"chat_result: {chat_result}")
+    assert isinstance(chat_result, AIMessage)
+    assert isinstance(chat_result.content, str)
+    assert "Paris" in chat_result.content
