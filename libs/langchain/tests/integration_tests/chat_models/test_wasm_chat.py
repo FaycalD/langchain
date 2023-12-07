784922caff76c9d8ba41a4404a9a6db8eab8f3ae
Xin Liu <sam@secondstate.io>
2023-12-07 17:54:13 +0000

    chore: update `test_wasm_chat.py`
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- libs/langchain/tests/integration_tests/chat_models/test_wasm_chat.py
+++ libs/langchain/tests/integration_tests/chat_models/test_wasm_chat.py
@@ -70,7 +70,7 @@ def test_chat_wasm() -> None:
     assert isinstance(chat_result.content, str)
     assert "Paris" in chat_result.content
 
-    # remove_downloaded(model_file)
+    remove_downloaded(model_file)
 
 
 @pytest.mark.enable_socket
@@ -94,7 +94,7 @@ def test_chat_wasm_with_wasm_file() -> None:
     assert isinstance(chat_result.content, str)
     assert "Paris" in chat_result.content
 
-    # remove_downloaded(model_file)
+    remove_downloaded(model_file)
 
 
 @pytest.mark.enable_socket
@@ -117,46 +117,22 @@ def test_chat_wasm_with_reverse_prompt() -> None:
     assert isinstance(chat_result.content, str)
     assert "Paris" in chat_result.content
 
-    # remove_downloaded(model_file)
+    remove_downloaded(model_file)
 
 
-# @pytest.mark.enable_socket
-# def test_chat_wasm_service() -> None:
-#     """This test requires the port 8080 is not occupied."""
-#     model_file = get_assets(
-#         "https://huggingface.co/second-state/TinyLlama-1.1B-Chat-v0.3-GGUF/resolve/main/tinyllama-1.1b-chat-v0.3.Q5_K_M.gguf"
-#     )
-#     print(f"******* model_file: {model_file}")
-#     api_server_wasm_file = get_assets(
-#         "https://github.com/second-state/llama-utils/raw/main/api-server/llama-api-server.wasm"
-#     )
+@pytest.mark.enable_socket
+def test_chat_wasm_service() -> None:
+    """This test requires the port 8080 is not occupied."""
 
-#     command = [
-#         "wasmedge",
-#         "--dir",
-#         ".:.",
-#         "--nn-preload",
-#         f"default:GGML:AUTO:{model_file}",
-#         "llama-api-server.wasm",
-#         "-p",
-#         "chatml",
-#     ]
-#     server_process = subprocess.Popen(command)
+    # a test service
+    service_ip_addr = "0.0.0.0"
+    service_port = "8080"
 
-#     # a test service
-#     service_ip_addr = "0.0.0.0"
-#     service_port = "8080"
-
-#     chat = ChatWasmService(service_ip_addr=service_ip_addr, service_port=service_port)
-#     system_message = SystemMessage(content="You are an AI assistant")
-#     user_message = HumanMessage(content="What is the capital of France?")
-#     messages = [system_message, user_message]
-#     response = chat(messages)
-#     assert isinstance(response, AIMessage)
-#     assert isinstance(response.content, str)
-#     assert "Paris" in response.content
-
-#     remove_downloaded(model_file)
-#     remove_downloaded(api_server_wasm_file)
-
-#     server_process.terminate()
+    chat = ChatWasmService(service_ip_addr=service_ip_addr, service_port=service_port)
+    system_message = SystemMessage(content="You are an AI assistant")
+    user_message = HumanMessage(content="What is the capital of France?")
+    messages = [system_message, user_message]
+    response = chat(messages)
+    assert isinstance(response, AIMessage)
+    assert isinstance(response.content, str)
+    assert "Paris" in response.content
